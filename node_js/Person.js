8dd202abf3b09ee0b317e14905b32dbe478d3331
Vladimir Lipunov <volo_lipu@yahoo.com>
2018-09-20 19:50:50 -0700

    added node js REST server

---
+++ node_js/Person.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(firstName, lastName, id, phone, street, city, state, zip) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.phone = phone;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+  }
+}
+
+module.exports = Person;
